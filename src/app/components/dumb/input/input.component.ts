3fd3039aa8b47d7f9c70cc87dfe5fbd36f228582
CrezyHamsterOd <baloon2003@gmail.com>
2019-07-11 10:47:01 +0300

    add dumb components
    *app-text-button
    *app-input
    *app-checkbox

---
+++ src/app/components/dumb/input/input.component.ts
@@ -0,0 +1,11 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.css']
+})
+export class InputComponent {
+  @Input() value = '';
+  @Output() valueChange = new EventEmitter<string>();
+}
