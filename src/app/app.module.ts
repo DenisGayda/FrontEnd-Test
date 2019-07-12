5ae576727d9589d8659ca093d59f2a59e4a0ab78
Shter <shtetina@rambler.ru>
2019-07-12 11:53:50 +0300

    лист отображения

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { TextButtonComponent } from './components/dumb/text-button/text-button.c
 import { InputComponent } from './components/dumb/input/input.component';
 import { CheckboxComponent } from './components/dumb/checkbox/checkbox.component';
 import {FormsModule} from '@angular/forms';
+import { ProfilePageComponent } from './components/smart/profile-page/profile-page.component';
+import { ListOfPassedTestsComponent } from './components/smart/list-of-passed-tests/list-of-passed-tests.component';
+import { ListComponent } from './components/dumb/list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TextButtonComponent,
     InputComponent,
-    CheckboxComponent
+    CheckboxComponent,
+    ProfilePageComponent,
+    ListOfPassedTestsComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
