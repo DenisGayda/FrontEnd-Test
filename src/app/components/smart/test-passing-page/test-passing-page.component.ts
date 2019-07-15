5664acd0db7e3cadee775776daf990915978bfa1
Shter <shtetina@rambler.ru>
2019-07-15 17:36:45 +0300

    страница прохождения теста

--- src/app/components/smart/test-passing-page/test-passing-page.component.ts
+++ src/app/components/smart/test-passing-page/test-passing-page.component.ts
@@ -1,12 +1,15 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
+import {TestInterface} from "../../../../config/interfaces/test.interface";
 
 @Component({
   selector: 'app-test-passing-page',
   templateUrl: './test-passing-page.component.html',
-  styleUrls: ['./test-passing-page.component.css']
+  styleUrls: ['./test-passing-page.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TestPassingPageComponent implements OnInit {
-  @Input() test: object[];
+  @Input() test: TestInterface;
+  questions: object[] = this.test.questions;
 
   constructor() {}
 
