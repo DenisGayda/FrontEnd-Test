848b95d7c0f5d4a6f0286e03faaf6af9d727a769
Shter <shtetina@rambler.ru>
2019-07-15 10:50:27 +0300

    лист отображения правка

--- src/app/components/dumb/list/list.component.ts
+++ src/app/components/dumb/list/list.component.ts
@@ -6,7 +6,7 @@ import {ChangeDetectionStrategy, Component, Input, TemplateRef} from '@angular/c
   styleUrls: ['./list.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ListComponent {
-  @Input() elements;
-  @Input() template: TemplateRef<any>;
+export class ListComponent<T> {
+  @Input() elements: T[];
+  @Input() template: TemplateRef<T>;
 }
