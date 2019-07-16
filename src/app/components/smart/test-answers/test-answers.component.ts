ccb117730ceed6dd2d15892d49aaba7324c41fba
Shter <shtetina@rambler.ru>
2019-07-16 16:31:18 +0300

    прохождение тестов правки

--- src/app/components/smart/test-answers/test-answers.component.ts
+++ src/app/components/smart/test-answers/test-answers.component.ts
@@ -1,10 +1,11 @@
-import {Component, Input} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
 import {Answer} from '../../../../config/interfaces/test.interface';
 
 @Component({
   selector: 'app-test-answers',
   templateUrl: './test-answers.component.html',
-  styleUrls: ['./test-answers.component.scss']
+  styleUrls: ['./test-answers.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TestAnswersComponent {
   @Input() answers: Answer[];
