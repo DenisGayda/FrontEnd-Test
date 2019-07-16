68e912a21fbc54f2f1965cc60f0700ec0786cec7
alexbliznec <a.yashuk@list.ru>
2019-07-16 13:08:07 +0300

    admin-page user-page tests-page test-passing-page not-found-page templates added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,11 @@ import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {HttpMockRequestInterceptor} from './interceptors/HttpMockRequestInterceptor';
 import { LoginPageComponent } from './components/pages/login-page/login-page.component';
 import { RegistrationPageComponent } from './components/pages/registration-page/registration-page.component';
+import { UserPageComponent } from './components/pages/user-page/user-page.component';
+import { TestsPageComponent } from './components/pages/tests-page/tests-page.component';
+import { TestPassingPageComponent } from './components/pages/test-passing-page/test-passing-page.component';
+import { AdminPageComponent } from './components/pages/admin-page/admin-page.component';
+import { NotFoundPageComponent } from './components/pages/not-found-page/not-found-page.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +26,12 @@ import { RegistrationPageComponent } from './components/pages/registration-page/
     CheckboxComponent,
     ListComponent,
     LoginPageComponent,
-    RegistrationPageComponent
+    RegistrationPageComponent,
+    UserPageComponent,
+    TestsPageComponent,
+    TestPassingPageComponent,
+    AdminPageComponent,
+    NotFoundPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/pages/test-passing-page/test-passing-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test-passing-page',
+  templateUrl: './test-passing-page.component.html',
+  styleUrls: ['./test-passing-page.component.scss']
+})
+export class TestPassingPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
