b6962d288741a4904250f66b100d5657044d2670
Alex <34835812+alexbliznec@users.noreply.github.com>
2019-07-22 17:51:39 +0300

    Update tests-page.component.ts

--- src/app/components/pages/tests-page/tests-page.component.ts
+++ src/app/components/pages/tests-page/tests-page.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-tests-page',
   templateUrl: './tests-page.component.html',
-  styleUrls: ['./tests-page.component.scss']
+  styleUrls: ['./tests-page.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TestsPageComponent implements OnInit {
 
