7e43a4649cc0186f94b463c9295f1f2ef440d734
Alex <34835812+alexbliznec@users.noreply.github.com>
2019-07-22 17:52:26 +0300

    Update user-page.component.ts

--- src/app/components/pages/user-page/user-page.component.ts
+++ src/app/components/pages/user-page/user-page.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-user-page',
   templateUrl: './user-page.component.html',
-  styleUrls: ['./user-page.component.scss']
+  styleUrls: ['./user-page.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class UserPageComponent implements OnInit {
 
